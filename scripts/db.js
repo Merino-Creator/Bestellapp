36e2bad09ce3f1fc8cbdbf293ec8f3799629c027
Daniel <stuermer.dan137@gmail.com>
2026-01-02 10:35:40 +0100

    created database

---
+++ scripts/db.js
@@ -0,0 +1,62 @@
+dishes = [
+    {
+        "name": "Veggie mushroom black burger",
+        "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        "price": 16.90
+    },
+    {
+        "name": "All meat burger",
+        "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+        "price": 15.90
+    },
+    {
+        "name": "Beef red burger",
+        "description": "Beef, Cheese, Tomatoes, Lettuce, Onions",
+        "price": 14.90
+    },
+    {
+        "name": "Big Chicken Burger",
+        "description": "Chicken, Cheese, Tomatoes, Lettuce, Onions, Bell pepper",
+        "price": 15.90
+    },
+    {
+        "name": "Pizza Margherita",
+        "description": "Tomato sauce, Mozzarella",
+        "price": 11.90
+    },
+    {
+        "name": "Pizza Chorizo",
+        "description": "Tomato slices, Mozzarella, Chorizo",
+        "price": 13.90
+    },
+    {
+        "name": "Pizza Funghi",
+        "description": "Red onions, Olives, Button mushrooms, Mozzarella",
+        "price": 12.90
+    },
+    {
+        "name": "Pizza Quattro Fromaggi with Chicken",
+        "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+        "price": 15.90
+    },
+    {
+        "name": "Warm beef arugula salad",
+        "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried tomatoes, Balsamic-vinegar dressing",
+        "price": 16.90
+    },
+    {
+        "name": "Mini green salad",
+        "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+        "price": 7.90
+    },
+    {
+        "name": "Green salad with sea food",
+        "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+        "price": 16.90
+    },
+    {
+        "name": "Vegan green salad with tofu",
+        "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        "price": 14.90
+    },
+]
