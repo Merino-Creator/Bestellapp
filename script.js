819bccf0e631eb9232762dfe854bf39e82346718
Daniel <stuermer.dan137@gmail.com>
2026-01-20 20:46:38 +0100

    deleted unnecessary js file

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ function renderMenu(type, containerId) {
 function toggleMenu(x) {
     x.classList.toggle("change");
 }
+
+function saveToLocalStorage() {
+    localStorage.setItem("basket", JSON.stringify(basket));
+}
+
+function getFromLocalStorage() {
+    let newBasket = JSON.parse(localStorage.getItem("basket"));
+
+    if (newBasket) {
+        basket = newBasket;
+    }
+}
